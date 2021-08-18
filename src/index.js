1b0392f877fa25bd229bc6f1c4896794f90119c4
Svetlana Drozdova <sdrozdova032@gmail.com>
2021-08-18 16:17:52 +0400

    init: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+let number = 0;
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n*-1;
+        number = n.toString().split('').reverse().join('');
+    } else {
+        number = n.toString().split('').reverse().join('');
+    }
+    return number;
 }
